9bbdc044b6dfacc7c2d4483559356456a71b5866
Micah Bales <micahbales@gmail.com>
2017-11-06 22:53:47 -0500

    Access Data from Giphy and Return JSON
    The Express server now has one route, which handles a
    request for data from the Giphy API using the secret key stored
    server-side.
    
    The search term is passed via the URL's 'search' query (defaults
    to a blank string). For the time being, we've hard-coded the number
    of gif records that we want Giphy to return: 3. Our gifs should be
    PG-rated.

--- giphy-server/giphy-server.js
+++ giphy-server/giphy-server.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
 
 require('dotenv').config();
 
-app.get('/api/v1/giphy', (req, res) => {
-  /* get data from the GIPHY API
-     and return it to the Vue app */
-  // res.json({});
+/*
+   Query the Giphy API server-side,
+   to avoid exposing credentials
+   Return results to our Vue front end
+                                       */
+app.get('/api/v1/giphy', async (req, res) => {
+  try {
+    const gifs = await axios.get(
+      `http://api.giphy.com/v1/gifs/search` +
+      `?api_key=${process.env.GIPHY_API_KEY}` +
+      `&q=${req.query.search || ''}` +
+      `&limit=3` +
+      `&rating=pg`)
+    .then((res) => { return res.data });
+
+    res.json(gifs);
+
+  } catch(err) { console.error(err); }
 });
 
 app.listen(3000, () => {
