764fb322cb6663615b7cdc542b5015841820ab50
Micah Bales <micahbales@gmail.com>
2017-11-07 11:42:25 -0500

    Get Gif URLs from Giphy

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
   <div id="app">
+    <form v-on:submit="searchGiphy">
+      <input type="text" v-model="searchString" placeholder="GIF ME!"/>
+      <button type="submit">Go</button>
+    </form>
+    <h3>{{searchString}}</h3>
+    <div v-for="gif in gifs">
+      <span>
+        {{gif.images.original.url}}
+      </span>
+    </div>
   </div>
 </template>
 
@@ -8,6 +18,24 @@
 export default {
   name: 'app',
   components: {},
+  data() {
+    return {
+      searchString: '',
+      gifs: [],
+    };
+  },
+  methods: {
+    searchGiphy(e) {
+      e.preventDefault();
+      this.$http.get('http://localhost:3000' +
+        `/api/v1/giphy?search=${this.searchString}`)
+      .then((res) => {
+        res.body.data.map(gif => this.gifs.push(gif));
+      }, (err) => {
+        console.error(err);
+      });
+    },
+  },
 };
 </script>
 
