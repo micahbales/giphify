936973187fa732b5e34e3b4ec43bc50abada98bc
Micah Bales <micahbales@gmail.com>
2017-11-07 11:07:33 -0500

    Add VueResource To Allow HTTP Requests by Vue

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueResource from 'vue-resource';
 import App from './App';
+
+Vue.use(VueResource);
 
 Vue.config.productionTip = false;
 
@@ -9,5 +12,5 @@ Vue.config.productionTip = false;
 new Vue({
   el: '#app',
   template: '<App/>',
-  components: { App },
+  components: { App, VueResource },
 });
