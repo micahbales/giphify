76663b2f94c49bbb84a2daef7c5c849272c6a5b0
Micah Bales <micahbales@gmail.com>
2017-11-08 12:09:21 -0500

    Add Logo and Some Styles
    This is the first run-through on styling the app. We added a logo
    that disappears when the user begins to type their query. This leaves
    more space to view the gifs that are returned.
    
    There is still some work to do on styling the individual images, as
    well as figuring out some general positioning questions.

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,88 @@
 <template>
   <div id="app" class="container">
-    <div class="row">
-      <form v-on:submit="searchGiphy" class="col-xs-12">
-        <input type="text" v-model="searchString" placeholder="GIF ME!"/>
-        <button type="submit" class="btn">Go</button>
-      </form>
-      <h2>{{searchString}}</h2>
-      <GifDisplay :gifs="gifs" />
+    <div v-if="!searchString" class="row logo">
+      <img src="./assets/img/giphy-logo.gif"/>
     </div>
+    <div class="row">
+      <form v-on:submit="searchGiphy" class="col-xs-12 search-form">
+        <input type="text" v-model="searchString" placeholder="Search for GIFs"/><br/>
+        <button type="submit" class="btn btn--search">Go!</button>
+      </form>
+      <h2 class="col-xs-12 search-string">{{searchString}}</h2>
+    </div>
+    <GifDisplay :gifs="gifs" />
   </div>
 </template>
 
 <script>
-import GifDisplay from './components/GifDisplay';
+  import GifDisplay from './components/GifDisplay';
 
-export default {
-  name: 'app',
-  components: { GifDisplay },
-  data() {
-    return {
-      searchString: '',
-      gifs: [],
-    };
-  },
-  methods: {
-    searchGiphy(e) {
-      e.preventDefault();
-      this.$http.get(`/api/v1/giphy?search=${this.searchString}`)
-      .then((res) => {
-        this.gifs = [];
-        res.body.data.map(gif => this.gifs.push(gif));
-      }, (err) => {
-        console.error(err);
-      });
+  export default {
+    name: 'app',
+    components: { GifDisplay },
+    data() {
+      return {
+        searchString: '',
+        gifs: [],
+      };
     },
-  },
-};
+    methods: {
+      searchGiphy(e) {
+        e.preventDefault();
+        this.$http.get(`/api/v1/giphy?search=${this.searchString}`)
+        .then((res) => {
+          this.gifs = [];
+          res.body.data.map(gif => this.gifs.push(gif));
+        }, (err) => {
+          console.error(err);
+        });
+      },
+    },
+  };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    background-color: #2c3e50!important; /* overriding bootstrap */
+  }
+
+  input[type=text] {
+    text-align: center;
+    border: 2px solid #fefefe;
+    border-radius: 1px;
+    background: #85c578;
+    height: 40px;
+    width: 200px;
+  }
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #fefefe;
+    margin-top: 60px;
+  }
+
+  .logo img {
+    height: 150px;
+  }
+
+  .search-form {
+    padding: 20px 0px;
+    transform: translateX(.6%);
+  }
+
+  .btn--search {
+    background-color: #ef676a;
+    border-radius: 1px;
+    height: 30px;
+    width: 200px;
+  }
+
+  h2.search-string {
+    max-width: 320px;
+    margin: 0 auto;
+    float: none;
+  }
 </style>
--- src/components/GifDisplay.vue
+++ src/components/GifDisplay.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="row gif-display">
-    <div v-for="gif in gifs" class="gif col-sm-12 col-md-6 col-lg-3">
+    <div v-for="gif in gifs" class="gif col-xs-12 center-block">
       <img :src="gif.images.original.url"/>
     </div>
   </div>
@@ -13,4 +13,11 @@
 </script>
 
 <style scoped>
+.center-block {
+  float: none;
+}
+
+.gif {
+  padding: 15px 0px;
+}
 </style>
