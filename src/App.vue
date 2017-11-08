9a3afd4469f6f0c7d572c8322827ad444ab4002e
Micah Bales <micahbales@gmail.com>
2017-11-08 09:33:18 -0500

    Add GifDisplay Component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,22 @@
 <template>
-  <div id="app">
-    <form v-on:submit="searchGiphy">
-      <input type="text" v-model="searchString" placeholder="GIF ME!"/>
-      <button type="submit">Go</button>
-    </form>
-    <h3>{{searchString}}</h3>
-    <div v-for="gif in gifs">
-      <span>
-        {{gif.images.original.url}}
-      </span>
+  <div id="app" class="container">
+    <div class="row">
+      <form v-on:submit="searchGiphy" class="col-xs-12">
+        <input type="text" v-model="searchString" placeholder="GIF ME!"/>
+        <button type="submit" class="btn">Go</button>
+      </form>
+      <h2>{{searchString}}</h2>
+      <GifDisplay :gifs="gifs" />
     </div>
   </div>
 </template>
 
 <script>
+import GifDisplay from './components/GifDisplay';
 
 export default {
   name: 'app',
-  components: {},
+  components: { GifDisplay },
   data() {
     return {
       searchString: '',
@@ -29,6 +28,7 @@ export default {
       e.preventDefault();
       this.$http.get(`/api/v1/giphy?search=${this.searchString}`)
       .then((res) => {
+        this.gifs = [];
         res.body.data.map(gif => this.gifs.push(gif));
       }, (err) => {
         console.error(err);
---
+++ src/components/GifDisplay.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="row gif-display">
+    <div v-for="gif in gifs" class="gif col-sm-12 col-md-6 col-lg-3">
+      <img :src="gif.images.original.url"/>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['gifs'],
+  };
+</script>
+
+<style scoped>
+</style>
